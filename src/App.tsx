56f298a3ef7ea04e64fc721e6685cc58bb28a73c
Chamila Wickramasinghe <chamilawickramasinghe@Thinukas-iMac.local>
2025-10-16 23:50:05 +1100

    Version 4 - Force docs update

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Contact from './pages/Contact'
 import Admin from './pages/Admin'
 
 function App() {
-  // Updated with HashRouter for GitHub Pages compatibility - Version 3 - FIXED
+  // Updated with HashRouter for GitHub Pages compatibility - Version 4 - FINAL FIX
   return (
     <BookingProvider>
       <Router>
