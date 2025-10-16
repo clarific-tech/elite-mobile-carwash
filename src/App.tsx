118f505288928c947e7344f3295a18c3464a1174
Chamila Wickramasinghe <chamilawickramasinghe@Thinukas-iMac.local>
2025-10-16 23:50:58 +1100

    Switch to BrowserRouter with basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { BookingProvider } from './context/BookingContext'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -13,7 +13,7 @@ function App() {
   console.log('App loaded with HashRouter');
   return (
     <BookingProvider>
-      <Router>
+      <Router basename="/elite-mobile-carwash">
         <div className="min-h-screen bg-background">
           <Header />
           <main>
