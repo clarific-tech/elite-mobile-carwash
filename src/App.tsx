18a9ae44a1d46863ec97ffb1f497adcea0b6232d
Chamila Wickramasinghe <chamilawickramasinghe@Thinukas-iMac.local>
2025-10-16 23:36:01 +1100

    Simplify App to show only Home component for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { BookingProvider } from './context/BookingContext'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
-import Services from './pages/Services'
-import Booking from './pages/Booking'
-import Contact from './pages/Contact'
-import Admin from './pages/Admin'
 
 function App() {
   return (
     <BookingProvider>
-      <Router>
-        <div className="min-h-screen bg-background">
-          <Header />
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/booking" element={<Booking />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/admin" element={<Admin />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+      <div className="min-h-screen bg-background">
+        <Header />
+        <main>
+          <Home />
+        </main>
+        <Footer />
+      </div>
     </BookingProvider>
   )
 }
