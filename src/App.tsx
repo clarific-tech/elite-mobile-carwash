5d9ef5fe5f0511026ea318f0c9675bee7a73cdc5
Chamila Wickramasinghe <chamilawickramasinghe@Thinukas-iMac.local>
2025-10-16 23:26:26 +1100

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { BookingProvider } from './context/BookingContext'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -11,7 +11,7 @@ import Admin from './pages/Admin'
 function App() {
   return (
     <BookingProvider>
-      <Router basename="/elite-mobile-carwash">
+      <Router>
         <div className="min-h-screen bg-background">
           <Header />
           <main>
