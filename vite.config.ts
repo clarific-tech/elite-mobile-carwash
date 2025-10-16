f320c861179a6706d4285a4980d361012ad17588
Chamila Wickramasinghe <chamilawickramasinghe@Thinukas-iMac.local>
2025-10-17 01:15:01 +1100

    Update docs with Hostinger-ready build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/elite-mobile-carwash/' : '/',
+  base: '/',
   build: {
     rollupOptions: {
       output: {
